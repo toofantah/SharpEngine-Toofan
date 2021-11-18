210ea4b867989fafa8fc41273f562b567d2dfe56
toofantah <toofan.tah@gmail.com>
2021-11-18 12:40:58 +0300

    Update

---
+++ Transform.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SharpEngine
+{
+	public class Transform
+	{
+		public Vector CurrentScale { get; set; }
+		public Vector Position { get; set; }
+		public Vector Rotation { get; set; }
+		public Matrix Matrix => Matrix.Translation(Position) * Matrix.Rotation(Rotation) * Matrix.Scale(CurrentScale);
+
+		public Vector Forward => Matrix.Transform(Matrix, Vector.Forward, 0);
+		public Vector Backward => Matrix.Transform(Matrix, Vector.Backward, 0);
+		public Vector Left => Matrix.Transform(Matrix, Vector.Left, 0);
+		public Vector Right => Matrix.Transform(Matrix, Vector.Right, 0);
+
+		public Transform()
+		{
+			this.CurrentScale = new Vector(1, 1, 1);
+		}
+
+		public void Scale(float multiplier)
+		{
+			CurrentScale *= multiplier;
+		}
+
+		public void Move(Vector direction)
+		{
+			this.Position += direction;
+		}
+
+		public void Rotate(float zAngle)
+		{
+			var rotation = this.Rotation;
+			rotation.z += zAngle;
+			this.Rotation = rotation;
+		}
+	}
+}
---
+++ Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SharpEngine
+{
+	public class Triangle : Shape
+	{
+		public Triangle(Material material) : base(CreateTriangle(), material)
+		{
+		}
+
+		static Vertex[] CreateTriangle()
+		{
+			const float scale = .1f;
+			float height = MathF.Sqrt(0.75f) * scale;
+			return new Vertex[] {
+				new Vertex(new Vector(-scale, -height/2), Color.Red),
+				new Vertex(new Vector(scale, -height/2), Color.Green),
+				new Vertex(new Vector(0f, height), Color.Blue)
+			};
+		}
+	}
+}
